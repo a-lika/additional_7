6131081c33e4242bbacf12b809e560c895167c88
Anzhelika <anzhelika110k@gmail.com>
2018-10-08 15:18:11 +0300

    feat: 12/12 tests passing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = function solveSudoku(matrix) {
-    const variables = []; 
+    const variables = [];
     let solved = true;
 
     for (let i = 0; i < 9; i++) {
@@ -9,61 +9,61 @@ module.exports = function solveSudoku(matrix) {
                 variables[i][j] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
             } else {
                 variables[i][j] = [matrix[i][j]];
-                }
-        }
-    }
- 
-    while(solved)  {
-        solved = false;
-            for (let i = 0; i < 9; i++) {
-                for (let j = 0; j < 9; j++) {
-                    if(matrix[i][j] === 0) {
-                        setVariablesRange(i,j); 
-                 }
             }
         }
-         
-        if(!solved) {
+    }
+
+    while (solved) {
+        solved = false;
+        for (let i = 0; i < 9; i++) {
+            for (let j = 0; j < 9; j++) {
+                if (matrix[i][j] === 0) {
+                    setVariablesRange(i, j);
+                }
+            }
+        }
+
+        if (!solved) {
             loop: for (let i = 0; i < 9; i++) {
                 for (let j = 0; j < 9; j++) {
                     if (matrix[i][j] === 0) {
-                           matrix[i][j] = variables[i][j][0];
-                           solved = true;
-                           break loop;
-                      } 
-                  }
+                        matrix[i][j] = variables[i][j][0];
+                        solved = true;
+                        break loop;
+                    }
+                }
             }
         }
-     };
-     
-     function setVariablesRange(i,j)	{
-         for (let k = 0; k < 9; k++) {
+    };
+
+    function setVariablesRange(i, j) {
+        for (let k = 0; k < 9; k++) {
             if (variables[i][j].indexOf(matrix[i][k]) != -1) {
-                 variables[i][j].splice(variables[i][j].indexOf(matrix[i][k]), 1);
-             }
+                variables[i][j].splice(variables[i][j].indexOf(matrix[i][k]), 1);
+            }
             if (variables[i][j].indexOf(matrix[k][j]) != -1) {
                 variables[i][j].splice(variables[i][j].indexOf(matrix[k][j]), 1);
-            }	
-         }
-         setNumbers(i,j);	
+            }
+        }
+        setNumbers(i, j);
     };
-	
-    function setNumbers(i,j) {
-	   let rangeI = Math.floor(i/3)*3;
-	   let rangeJ = Math.floor(j/3)*3;
-	   for (let n = rangeI; n < rangeI + 3; n++ ) {
-           for (let l = rangeJ; l < rangeJ + 3; l++ ) {
-               if (variables[i][j].indexOf(matrix[n][l]) != -1) {
-                   variables[i][j].splice(variables[i][j].indexOf(matrix[n][l]), 1);
-               }
-           }  	
-	   }
 
-        if(variables[i][j].length == 1) {
-            matrix[i][j]=variables[i][j][0];
+    function setNumbers(i, j) {
+        let rangeI = Math.floor(i / 3) * 3;
+        let rangeJ = Math.floor(j / 3) * 3;
+        for (let n = rangeI; n < rangeI + 3; n++) {
+            for (let l = rangeJ; l < rangeJ + 3; l++) {
+                if (variables[i][j].indexOf(matrix[n][l]) != -1) {
+                    variables[i][j].splice(variables[i][j].indexOf(matrix[n][l]), 1);
+                }
+            }
+        }
+
+        if (variables[i][j].length == 1) {
+            matrix[i][j] = variables[i][j][0];
             solved = true;
-        } 
-        
+        }
+
         for (let k = 0; k < variables[i][j].length; k++) {
             let s = 0;
             for (let n = 0; n < 9; n++) {
@@ -79,7 +79,7 @@ module.exports = function solveSudoku(matrix) {
                 }
             }
         }
-        
+
         for (let k = 0; k < variables[i][j].length; k++) {
             let s = 0;
             for (let n = 0; n < 9; n++) {
@@ -95,16 +95,16 @@ module.exports = function solveSudoku(matrix) {
                 }
             }
         }
-    
-        for (let k = 0; k < variables[i][j].length; k++) {   
+
+        for (let k = 0; k < variables[i][j].length; k++) {
             let s = 0;
             let applicants = [variables[i][j][k]];
 
-	        for (let n = rangeI; n < rangeI + 3; n++ ) {
-                for (let m = rangeJ; m < rangeJ + 3; m++ ) {
+            for (let n = rangeI; n < rangeI + 3; n++) {
+                for (let m = rangeJ; m < rangeJ + 3; m++) {
                     if (variables[n][m].indexOf(variables[i][j][k]) != -1) {
                         s++;
-                            applicants.push([n, m]);
+                        applicants.push([n, m]);
                     }
                 }
                 if (n == rangeI + 2) {
@@ -112,11 +112,15 @@ module.exports = function solveSudoku(matrix) {
                         matrix[i][j] = variables[i][j][k];
                         variables[i][j] = [matrix[i][j]];
                         solved = true;
+                    } else if (s <= 3) {
+                        matrix[i][j] = {};
+                        variables[i][j] = [matrix[i][j]];
+                        solved = true;
                     }
                 }
             }
         }
     };
-   
+
     return matrix;
 };
